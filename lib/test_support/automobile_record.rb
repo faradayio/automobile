ae09310d93f7650d7adc3895c801df07d60fcb25
Ian Hough <ian@brighterplanet.com>
2010-11-05 17:45:50 -0700

    NOTE: this commit depends on the latest audit_prep branch of earth. model -> make_model, model_year -> make_model_year, variant -> make_model_year_variant, fuel_efficiency_multiplier -> hybridity_multiplier. Also cleaning up fuel_efficiency and annual_distance committees.

--- lib/test_support/automobile_record.rb
+++ lib/test_support/automobile_record.rb
@@ -19,12 +19,13 @@ class AutomobileRecord < ActiveRecord::Base
   include BrighterPlanet::Automobile
   include Sniff::Emitter
   
-  belongs_to :variant,    :class_name => 'AutomobileVariant'
-  belongs_to :make,       :class_name => 'AutomobileMake'
-  belongs_to :model,      :class_name => 'AutomobileModel'
-  belongs_to :model_year, :class_name => 'AutomobileModelYear'
-  belongs_to :fuel_type,  :class_name => 'AutomobileFuelType'
-  belongs_to :size_class, :class_name => 'AutomobileSizeClass'
+  belongs_to :make,                    :class_name => 'AutomobileMake',                 :foreign_key => 'make_name'
+  belongs_to :make_year,               :class_name => 'AutomobileMakeYear',             :foreign_key => 'make_year_name'
+  belongs_to :make_model,              :class_name => 'AutomobileMakeModel',            :foreign_key => 'make_model_name'
+  belongs_to :make_model_year,         :class_name => 'AutomobileMakeModelYear',        :foreign_key => 'make_model_year_name'
+  belongs_to :make_model_year_variant, :class_name => 'AutomobileMakeModelYearVariant', :foreign_key => 'make_model_year_variant_row_hash'
+  belongs_to :fuel_type,               :class_name => 'AutomobileFuelType',             :foreign_key => 'fuel_type_code'
+  belongs_to :size_class,              :class_name => 'AutomobileSizeClass'
   
   falls_back_on :fuel_efficiency => 20.182.miles_per_gallon.to(:kilometres_per_litre), # mpg https://brighterplanet.sifterapp.com/projects/30/issues/428
                 :urbanity => 0.43, # EPA via Ian https://brighterplanet.sifterapp.com/projects/30/issues/428
