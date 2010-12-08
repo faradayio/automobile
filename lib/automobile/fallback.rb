2250c72a31e771d0e685fe7b8c903ac611d2b2f6
Ian Hough <ian@brighterplanet.com>
2010-12-08 10:26:17 -0500

    Fallback values are coded in the quorums

--- lib/automobile/fallback.rb
+++ lib/automobile/fallback.rb
@@ -2,9 +2,7 @@ module BrighterPlanet
   module Automobile
     module Fallback
       def self.included(base)
-        base.falls_back_on  :fuel_efficiency => 20.182.miles_per_gallon.to(:kilometres_per_litre), # mpg https://brighterplanet.sifterapp.com/projects/30/issues/428
-                            :urbanity => 0.43, # EPA via Ian https://brighterplanet.sifterapp.com/projects/30/issues/428
-                            :annual_distance => 11819.miles.to(:kilometres) # miles https://brighterplanet.sifterapp.com/projects/30/issues/428
+        base.falls_back_on
       end
     end
   end
