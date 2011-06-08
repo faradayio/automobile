d8b3a797ec1e7a88efe676a2cec80e088a779a9a
Seamus Abshere <seamus@abshere.net>
2011-06-08 17:24:42 -0500

    create_table block is now separate from data_miner

--- lib/automobile/data.rb
+++ lib/automobile/data.rb
@@ -2,37 +2,35 @@ module BrighterPlanet
   module Automobile
     module Data
       def self.included(base)
+        base.create_table do
+          string   'make_name'
+          string   'make_year_name'
+          string   'make_model_name'
+          string   'make_model_year_name'
+          string   'make_model_year_variant_row_hash'
+          string   'size_class_name'
+          string   'automobile_fuel_name'
+          boolean  'hybridity'
+          float    'urbanity' # DEPRECATED
+          float    'urbanity_estimate'
+          float    'speed'
+          float    'city_speed'
+          float    'highway_speed'
+          boolean  'hybridity'
+          float    'hybridity_multiplier'
+          float    'fuel_efficiency'
+          float    'annual_distance'
+          float    'weekly_distance'
+          float    'daily_distance'
+          float    'daily_duration'
+          date     'acquisition'
+          date     'retirement'
+        end
+        
         base.data_miner do
-          schema do
-            string   'make_name'
-            string   'make_year_name'
-            string   'make_model_name'
-            string   'make_model_year_name'
-            string   'make_model_year_variant_row_hash'
-            string   'size_class_name'
-            string   'automobile_fuel_name'
-            boolean  'hybridity'
-            float    'urbanity' # DEPRECATED
-            float    'urbanity_estimate'
-            float    'speed'
-            float    'city_speed'
-            float    'highway_speed'
-            boolean  'hybridity'
-            float    'hybridity_multiplier'
-            float    'fuel_efficiency'
-            float    'annual_distance'
-            float    'weekly_distance'
-            float    'daily_distance'
-            float    'daily_duration'
-            date     'acquisition'
-            date     'retirement'
-          end
-
           process "pull orphans" do
             AutomobileMakeFleetYear.run_data_miner! # sabshere 5/25/10 i'm not sure we actually need to have this in cm1
           end
-          
-          process :run_data_miner_on_belongs_to_associations
         end
       end
     end
