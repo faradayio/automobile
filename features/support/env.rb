4b03891430e334cca700c2490d69205a2096937f
Ian <ian@worklap.local>
2011-09-07 19:30:37 +0200

    Log test output

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,4 +8,4 @@ require 'data_miner'
 DataMiner.logger = Logger.new($stderr)
 
 require 'sniff'
-Sniff.init File.join(File.dirname(__FILE__), '..', '..'), :earth => [:automobile, :fuel], :cucumber => true
+Sniff.init File.join(File.dirname(__FILE__), '..', '..'), :earth => [:automobile, :fuel], :cucumber => true, :logger => 'log/test_log.txt'
