5471cd5476aa170080dae3751200aabbf4d9f282
Ian Hough <ijhough@gmail.com>
2011-10-21 17:21:01 +0200

    Revert "Revert "Re-write documentation and cleanup""
    This reverts commit 5d43fd13af1c015f99a3b1551810d542bb13ecd6.

--- lib/automobile/characterization.rb
+++ lib/automobile/characterization.rb
@@ -7,19 +7,20 @@ module BrighterPlanet
           has :model
           has :year
           has :size_class
-          has :automobile_fuel
+          has :automobile_fuel # can't call this 'fuel' or else sniff thinks it refers to Fuel not AutomobileFuel
           has :acquisition
           has :retirement
           has :hybridity
           has :urbanity
-          has :speed, :measures => Measurement::BigSpeed
-          has :city_speed, :measures => Measurement::BigSpeed
-          has :highway_speed, :measures => Measurement::BigSpeed
-          has :fuel_efficiency, :measures => Measurement::BigLengthPerVolume
-          has :annual_distance, :measures => Measurement::BigLength
+          has :city_speed,      :measures => Measurement::BigSpeed
+          has :highway_speed,   :measures => Measurement::BigSpeed
+          has :speed,           :measures => Measurement::BigSpeed
+          has :daily_duration,  :measures => :time
+          has :daily_distance,  :measures => Measurement::BigLength
           has :weekly_distance, :measures => Measurement::BigLength
-          has :daily_distance, :measures => Measurement::BigLength
-          has :daily_duration, :measures => :time
+          has :annual_distance, :measures => Measurement::BigLength
+          has :fuel_efficiency, :measures => Measurement::BigLengthPerVolume
+          has :fuel_use,        :measures => Measurement::Volume
         end
       end
     end
