97a6f5eec4715cc66ccb3701f4a57e72d936bf1f
Ian Hough <ijhough@gmail.com>
2012-06-18 16:17:34 +0200

    Update description of emission calculation scope

--- lib/automobile.rb
+++ lib/automobile.rb
@@ -3,6 +3,6 @@ require 'emitter'
 module BrighterPlanet
   module Automobile
     extend BrighterPlanet::Emitter
-    scope 'The automobile emission estimate is the total anthropogenic emissions from fuel and air conditioning used by the automobile during the timeframe. It includes CO2 emissions from combustion of non-biogenic fuel, CH4 and N2O emissions from combustion of all fuel, and fugitive HFC emissions from air conditioning.'
+    scope 'The automobile emission estimate is the total anthropogenic emissions from fuel and air conditioning used by the automobile during the timeframe. It includes CO2 emissions from combustion of non-biogenic fuel, CH4 and N2O emissions from combustion of all fuel, and fugitive HFC emissions from air conditioning. For vehicles powered by grid electricity it includes the emissions from generating that electricity.'
   end
 end
